bcebe51087c30f0af3db896944eba41805a3dcee
Eiji Kawasaki <eiji.d.kawasaki@gmail.com>
2024-06-30 22:11:21 +0200

    data-pipeline update

--- data-pipeline/predict.py
+++ data-pipeline/predict.py
@@ -2,8 +2,8 @@ import pandas as pd
 from pycaret.classification import *
 
 if __name__ == "__main__":
-    df = pd.read_csv('data/transformed_labelled_train.csv')
+    df = pd.read_csv('data_outputs/transformed_labelled_train.csv')
     s = setup(data=df)
     model = create_model('xgboost')
-    predictions_df = predict_model(model, data=pd.read_csv('data/transformed_unlabelled_test.csv'), raw_score=True)
-    predictions_df.to_csv('data/xgboost_predictions.csv', index=False)
+    predictions_df = predict_model(model, data=pd.read_csv('data_outputs/transformed_unlabelled_test.csv'), raw_score=True)
+    predictions_df.to_csv('data_outputs/xgboost_predictions.csv', index=False)
--- data-pipeline/process_data.py
+++ data-pipeline/process_data.py
@@ -7,18 +7,21 @@ from pycaret.classification import *
 def main(file_name, is_labeled):
     df = pd.read_csv('data/' + file_name)
     if is_labeled:
-        kwargs = {'ignore_features': ['Name', 'Ticket', 'Cabin', 'Parch', 'Embarked']}
+        kwargs = {'ignore_features': ['Name', 'Ticket', 'Cabin', 'Embarked']}
         kwargs.update({'target': 'Survived'})
     else:
-        kwargs = {'ignore_features': ['Name', 'Ticket', 'Cabin', 'Parch']}
+        kwargs = {'ignore_features': ['Name', 'Ticket', 'Cabin']}
     s = setup(data=df,
               numeric_imputation='mean',
               categorical_features=['Sex'],
               remove_outliers=True,
               **kwargs)
-    transformed_df = s.get_config('dataset_transformed')
-    transformed_df.to_csv('data/transformed_' + file_name, index=False)
-
+    s.get_config('dataset_transformed').to_csv('data_outputs/transformed_' + file_name, index=False)
+    if is_labeled:
+        s.get_config('X_train_transformed').to_csv('data_outputs/X_train_transformed.csv', index=False)
+        s.get_config('y_train_transformed').to_csv('data_outputs/y_train_transformed.csv', index=False)
+        s.get_config('X_test_transformed').to_csv('data_outputs/X_test_transformed.csv', index=False)
+        s.get_config('y_test_transformed').to_csv('data_outputs/y_test_transformed.csv', index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
